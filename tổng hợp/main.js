cbd6407dc6577e7b9cbb599f9a8dec1dc1950d2b
PQHtobi <phanquochung2511@gmail.com>
2022-09-22 19:10:49 +0700

    .

---
+++ tổng hợp/main.js
@@ -0,0 +1,10 @@
+/*  Tổng hợp các thuộc tính quan trọng của array
+1. Array.isArray(value)
+kiểm tra xem giá trị truyền vào có phải là 1 array không
+2.Array.from()
+console.log(console.log(Array.from([1, 2, 3], x => x + x )));
+console.log(Array.from('PQH'));
+trả về từng giá trị của array
+3.
+ */
+
